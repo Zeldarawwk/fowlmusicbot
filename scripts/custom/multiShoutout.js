c1608baaa74b4bfcdf82b23701605b3a41bccad8
Zeldarawwk <zelda@rawwk.com>
2018-03-01 02:05:10 +0000

    Changed headers to plural when reflecting multiple people. Plural shoutouts are now one link per streamer for simplicity as requested.

--- scripts/custom/multiShoutout.js
+++ scripts/custom/multiShoutout.js
@@ -9,24 +9,25 @@
 	
 		if (command.equalsIgnoreCase('so')) {
 
-            var shoutoutHeader = '/me Make sure to follow this amazing person!!! ltcDerp ltcDerp ltcDerp ▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ '
+			var shoutoutHeader = '/me Make sure to follow this amazing person!!! ltcDerp ltcDerp ltcDerp ▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ '
+			var shoutoutHeaderPlural = '/me Make sure to follow these amazing people!!! ltcDerp ltcDerp ltcDerp ▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ '
 
 			if (args[7] !== undefined) {
-                $.say(shoutoutHeader + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[4] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[5] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[6] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[7] + ' ltcLove ltcLove ltcLove ');
+                $.say(shoutoutHeaderPlural + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[4] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[5] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[6] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[7] + ' ltcLove ltcLove ltcLove ');
 			} else if (args[6] !== undefined) {
-				$.say(shoutoutHeader + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[4] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[5] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[6] + ' ltcLove ltcLove ltcLove ');
+				$.say(shoutoutHeaderPlural + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[4] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[5] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[6] + ' ltcLove ltcLove ltcLove ');
 			} else if (args[5] !== undefined) {
-				$.say(shoutoutHeader + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[4] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[5] + ' ltcLove ltcLove ltcLove ');
+				$.say(shoutoutHeaderPlural + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[4] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[5] + ' ltcLove ltcLove ltcLove ');
 			} else if (args[4] !== undefined) {
-				$.say(shoutoutHeader + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[4] + ' ltcLove ltcLove ltcLove ');
+				$.say(shoutoutHeaderPlural + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[4] + ' ltcLove ltcLove ltcLove ');
 			} else if (args[3] !== undefined) {
-				$.say(shoutoutHeader + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ');
+				$.say(shoutoutHeaderPlural + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[3] + ' ltcLove ltcLove ltcLove ');
 			} else if (args[2] !== undefined) {
-				$.say(shoutoutHeader + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ');
+				$.say(shoutoutHeaderPlural + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[2] + ' ltcLove ltcLove ltcLove ');
 			} else if (args[1] !== undefined) {
-				$.say(shoutoutHeader + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ');
+				$.say(shoutoutHeaderPlural + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[1] + ' ltcLove ltcLove ltcLove ');
 			} else if (args[0] !== undefined) {
-				$.say(shoutoutHeader + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ');
+				$.say(shoutoutHeader + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ' + 'www.twitch.tv/' + args[0] + ' ltcLove ltcLove ltcLove ');
 			} else {
                 $.say("Gotta specify someone to shoutout ltcYe");
             }
